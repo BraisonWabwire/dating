aef8dc527bdde3d25ae79e3660be317bb5158bd6
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-08-07 17:14:06 +0300

    creating the welcome page

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFBDDD3),
+      appBar: AppBar(
+        backgroundColor: Color(0xFFE91E63), // Pink
+        title: Text('LoveConnect'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Center(
+              child: Text(
+                "Welcome to Tujuane Dating App",
+                textAlign: TextAlign.center,
+                style: GoogleFonts.lato(
+                  color: Color(0xFF212121),
+                  fontSize: 35,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              'Kenya\'s leading dating platform. Connect with singles, explore profiles, and start your journey to love today!',
+              style: GoogleFonts.lato(
+                color: Color(0xFF757575),
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Image.asset(
+            'assets/images/welcome.png',
+            height: 400,
+            width: 400,
+            fit: BoxFit.cover,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFFE91E63),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                onPressed: () {
+                  // Handle login action
+                },
+                child: Text(
+                  'Login',
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(width: 20), // spacing between buttons
+              OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Color(0xFFE91E63), width: 2),
+                  padding: EdgeInsets.symmetric(horizontal: 45, vertical: 15),
+                ),
+                onPressed: () {
+                  // Handle signup action
+                },
+                child: Text(
+                  'Signup',
+                  style: GoogleFonts.lato(
+                    color: Color(0xFFE91E63),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
